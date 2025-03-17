ea1190368e58c38c0c90fd9f408f0b2aeb891a23
Nowwar <nowwar666@gmail.com>
2025-03-18 00:57:37 +0300

    initial commit

---
+++ OddEvenNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace OddEvenNumbersApp
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            PrintNumbers("Numbers", num);
+            PrintNumbers("Even No", num.Where(n => IsEven(n)));
+            PrintNumbers("Odd No", num.Where(n => IsOdd(n)));
+
+            Console.ReadLine();
+
+
+        }
+        static void PrintNumbers(string titel, IEnumerable<int> numbers)
+        {
+            Console.Write($"{titel} : [");
+            foreach (var num in numbers)
+            {
+                Console.Write(num + ",");
+            }
+            Console.WriteLine("]");
+        }
+
+        static bool IsEven(int Number)
+        {
+            return Number % 2 == 0 ? true : false;
+        }
+        static bool IsOdd(int Number)
+        {
+            return !IsEven(Number);
+        }
+    }
+        
+}
+
